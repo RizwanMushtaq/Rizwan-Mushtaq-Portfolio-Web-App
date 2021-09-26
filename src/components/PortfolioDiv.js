e1eccc868db1effa04215668ffbf70eec1b41846
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-09-26 21:27:57 +0200

    updating project section

--- src/components/PortfolioDiv.js
+++ src/components/PortfolioDiv.js
@@ -16,6 +16,13 @@ import PrintMyTweetApp364 from '../images/PrintMyTweetApp/PrintMyTweetAppImage-3
 
 //Do IT Now App - PHP Images
 import DoItNowAppPHPImage from '../images/DoItNowApp_PHP/doitnow_php_1006-255.png'
+import DoItNowAppPHPImage914 from '../images/DoItNowApp_PHP/doitnow_php_914-255.png'
+import DoItNowAppPHPImage814 from '../images/DoItNowApp_PHP/doitnow_php_814-255.png'
+import DoItNowAppPHPImage714 from '../images/DoItNowApp_PHP/doitnow_php_714-255.png'
+import DoItNowAppPHPImage614 from '../images/DoItNowApp_PHP/doitnow_php_614-255.png'
+import DoItNowAppPHPImage664 from '../images/DoItNowApp_PHP/doitnow_php_664-255.png'
+import DoItNowAppPHPImage564 from '../images/DoItNowApp_PHP/doitnow_php_564-255.png'
+import DoItNowAppPHPImage364 from '../images/DoItNowApp_PHP/doitnow_php_364-255.png'
 
 export default function PortfolioDiv() {
 
@@ -45,7 +52,6 @@ export default function PortfolioDiv() {
                 </div>
                 <div className={styles.imageContainer}>
                     <picture>
-                        {/* <img src={PrintMyTweetAppProjectImage} alt="project img" /> */}
                         <source media="(min-width: 1280px)" srcSet={PrintMyTweetAppProjectImage} />
                         <source media="(min-width: 1180px)" srcSet={PrintMyTweetAppProjectImage} />
                         <source media="(min-width: 1080px)" srcSet={PrintMyTweetApp914} />
@@ -74,7 +80,16 @@ export default function PortfolioDiv() {
                 </div>
                 
                 <div className={styles.projectDescription}>
-                    <div>Web Application Features</div>
+                    <div>Technologies Used</div>
+                    <div className={styles.projectDescriptionDetails}>
+                        <ul>
+                            <li>Canvas API</li>
+                            <li>JavaScript</li>
+                            <li>CSS</li>
+                            <li>HTML</li>
+                        </ul>
+                    </div>
+                    <div>Application Features</div>
                     <div className={styles.projectDescriptionDetails}>
                         <ul>
                             <li>Generate Image of any Size</li>
@@ -92,10 +107,25 @@ export default function PortfolioDiv() {
             {/* 2nd Project ********************************************** */}
             <div className={styles.projectContainer}>
                 <div className={styles.projectHeading}>
-                    Do It Now App (MySQL_PHP_JavaScript_CSS_HTML)
+                    Do It Now App
                 </div>
                 <div className={styles.imageContainer}>
-                    <img src={DoItNowAppPHPImage} alt="project img"></img>
+                    <picture>
+                        <source media="(min-width: 1280px)" srcSet={DoItNowAppPHPImage} />
+                        <source media="(min-width: 1180px)" srcSet={DoItNowAppPHPImage} />
+                        <source media="(min-width: 1080px)" srcSet={DoItNowAppPHPImage914} />
+                        <source media="(min-width: 980px)" srcSet={DoItNowAppPHPImage814} />
+                        <source media="(min-width: 880px)" srcSet={DoItNowAppPHPImage714} />
+                        <source media="(min-width: 780px)" srcSet={DoItNowAppPHPImage614} />
+
+                        <source media="(min-width: 768px)" srcSet={DoItNowAppPHPImage614} />
+                        
+                        <source media="(min-width: 680px)" srcSet={DoItNowAppPHPImage614} />
+                        <source media="(min-width: 580px)" srcSet={DoItNowAppPHPImage664} />
+                        <source media="(min-width: 480px)" srcSet={DoItNowAppPHPImage564} />
+                        <source media="(min-width: 380px)" srcSet={DoItNowAppPHPImage364} />
+                        <img src={DoItNowAppPHPImage364} alt="project img" />
+                    </picture>
                 </div>
                 <div className={styles.buttonContainer}>
                     <button onClick={() => window.open('https://doitnow-mysql-php-js-css-herok.herokuapp.com/')}>Live Demo</button>
@@ -106,7 +136,34 @@ export default function PortfolioDiv() {
                 </div>
                 
                 <div className={styles.projectDescription}>
-                    Prject Description
+                    <div>Technologies Used</div>
+                    <div className={styles.projectDescriptionDetails}>
+                        <ul>
+                            <li>Front-End Development</li>
+                                <ul>
+                                    <li>JavaScript</li>
+                                    <li>CSS</li>
+                                    <li>HTML</li>
+                                </ul>
+                            <li>Back-End Development</li>
+                                <ul>
+                                    <li>PHP</li>
+                                    <li>MySQL</li>
+                                </ul>
+                        </ul>
+                    </div>
+                    <div>Application Features</div>
+                    <div className={styles.projectDescriptionDetails}>
+                        <ul>
+                            <li>Registration of user is done and user data is saved in backend</li>
+                            <li>User login functionality is added</li>
+                            <li>User can add ToDo items in list</li>
+                            <li>User can add ToDo items in folder to organize ToDo list</li>  
+                            <li>User can see ToDo list based to date selected in Dialog</li>
+                            <li>User can see Weekly ToDo list in Dialog</li>
+                            <li>User can delete ToDo item from list</li>
+                        </ul>
+                    </div>
                 </div>
 
             </div>
