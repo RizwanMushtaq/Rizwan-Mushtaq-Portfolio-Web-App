15755a474740e8d2d2953ca6a8e80de467d7cac2
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-09-26 18:47:06 +0200

    updating project section

--- src/components/PortfolioDiv.js
+++ src/components/PortfolioDiv.js
@@ -74,7 +74,18 @@ export default function PortfolioDiv() {
                 </div>
                 
                 <div className={styles.projectDescription}>
-                    Prject Description
+                    <div>Web Application Features</div>
+                    <div className={styles.projectDescriptionDetails}>
+                        <ul>
+                            <li>Generate Image of any Size</li>
+                            <li>Writing Text in Image</li>
+                            <li>Change Colors of Text and background of Image</li>
+                            <li>Insert random Circles in the background</li>  
+                            <li>Position your text in Image</li>
+                            <li>Insert Tweeter icon and Position anywhere inside Image</li>
+                            <li>Export Images as PNG or JPEG formats</li>
+                        </ul>
+                    </div>
                 </div>
 
             </div>
